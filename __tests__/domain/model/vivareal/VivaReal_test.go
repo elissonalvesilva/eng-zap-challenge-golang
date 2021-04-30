643002ff60592cd444aa74aad671edbbf1f4357b
Elisson Alves Silva <elissonalvessilva@192.168.1.3>
2021-04-29 22:59:25 -0400

    test: create test to vivareal domain

---
+++ __tests__/domain/model/vivareal/VivaReal_test.go
@@ -0,0 +1,66 @@
+package vivamodel
+
+import (
+	"reflect"
+	"testing"
+
+	mock "github.com/elissonalvesilva/eng-zap-challenge-golang/__tests__/mock"
+
+	viva "github.com/elissonalvesilva/eng-zap-challenge-golang/domain/model/vivareal"
+	modelError "github.com/elissonalvesilva/eng-zap-challenge-golang/shared/errors"
+
+	"github.com/elissonalvesilva/eng-zap-challenge-golang/domain/protocols"
+)
+
+func TestNewImovel(t *testing.T) {
+	type args struct {
+		imovel protocols.Imovel
+	}
+	tests := []struct {
+		name      string
+		args      args
+		want      protocols.Imovel
+		wantErr   bool
+		typeError interface{}
+	}{
+		{
+			"Should return a error if Total Rental Price Value is invalid",
+			args{
+				imovel: mock.MockImovelErrorInvalidRentalTotalPrice,
+			},
+			mock.MockImovelErrorInvalidRentalTotalPrice,
+			true,
+			modelError.InvalidTotalRentalPriceValue(mock.MockImovelErrorInvalidRentalTotalPrice.ID),
+		},
+		{
+			"Should return a error if MontlyCondoFee is invalid",
+			args{
+				imovel: mock.MockImovelInvalidMonthlycondofeeValue,
+			},
+			mock.MockImovelInvalidMonthlycondofeeValue,
+			true,
+			modelError.InvalidMonthlycondofeeValue(mock.MockImovelInvalidMonthlycondofeeValue.ID),
+		},
+		{
+			"Should return a created imovel and a error equals to nil if success",
+			args{
+				imovel: mock.MockImovelSuccess,
+			},
+			mock.MockImovelSuccess,
+			false,
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := viva.NewImovel(tt.args.imovel)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewImovel() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewImovel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ __tests__/mock/imovel-vivareal.mock.go
@@ -0,0 +1,135 @@
+package mock
+
+import (
+	"time"
+
+	"github.com/elissonalvesilva/eng-zap-challenge-golang/domain/protocols"
+)
+
+var MockImovelErrorInvalidRentalTotalPrice = protocols.Imovel{
+	Usableareas:   40,
+	Listingtype:   "list1",
+	Createdat:     time.Now(),
+	Listingstatus: "active",
+	ID:            "123",
+	Parkingspaces: 1,
+	Updatedat:     time.Now(),
+	Owner:         false,
+	Images:        []string{"image1"},
+	Address: protocols.Address{
+		City:         "city",
+		Neighborhood: "aa",
+		Geolocation: protocols.Geolocation{
+			Precision: "GEO",
+			Location: protocols.Location{
+				Lat: 1231313,
+				Lon: 1213131,
+			},
+		},
+	},
+	Bathrooms: 1,
+	Bedrooms:  1,
+	Pricinginfos: protocols.PricingInfos{
+		Price:            0,
+		Yearlyiptu:       11233,
+		RentalTotalPrice: 0,
+		Businesstype:     "SALE",
+		Monthlycondofee:  123131,
+	},
+}
+
+var MockImovelInvalidMonthlycondofeeValue = protocols.Imovel{
+	Usableareas:   150,
+	Listingtype:   "list1",
+	Createdat:     time.Now(),
+	Listingstatus: "active",
+	ID:            "123",
+	Parkingspaces: 1,
+	Updatedat:     time.Now(),
+	Owner:         false,
+	Images:        []string{"image1"},
+	Address: protocols.Address{
+		City:         "city",
+		Neighborhood: "aa",
+		Geolocation: protocols.Geolocation{
+			Precision: "GEO",
+			Location: protocols.Location{
+				Lat: 1231313,
+				Lon: 1213131,
+			},
+		},
+	},
+	Bathrooms: 1,
+	Bedrooms:  1,
+	Pricinginfos: protocols.PricingInfos{
+		Price:            45000,
+		Yearlyiptu:       11233,
+		RentalTotalPrice: 12311313,
+		Businesstype:     "SALE",
+		Monthlycondofee:  0,
+	},
+}
+
+var MockImovelNotElegibleMonthlycondofeeValue = protocols.Imovel{
+	Usableareas:   150,
+	Listingtype:   "list1",
+	Createdat:     time.Now(),
+	Listingstatus: "active",
+	ID:            "123",
+	Parkingspaces: 1,
+	Updatedat:     time.Now(),
+	Owner:         false,
+	Images:        []string{"image1"},
+	Address: protocols.Address{
+		City:         "city",
+		Neighborhood: "aa",
+		Geolocation: protocols.Geolocation{
+			Precision: "GEO",
+			Location: protocols.Location{
+				Lat: -23.553723,
+				Lon: -46.631146,
+			},
+		},
+	},
+	Bathrooms: 1,
+	Bedrooms:  1,
+	Pricinginfos: protocols.PricingInfos{
+		Price:            45000,
+		Yearlyiptu:       11233,
+		RentalTotalPrice: 1,
+		Businesstype:     "SALE",
+		Monthlycondofee:  123131,
+	},
+}
+
+var MockImovelNotElegibleMaxRentalValue = protocols.Imovel{
+	Usableareas:   150,
+	Listingtype:   "list1",
+	Createdat:     time.Now(),
+	Listingstatus: "active",
+	ID:            "123",
+	Parkingspaces: 1,
+	Updatedat:     time.Now(),
+	Owner:         false,
+	Images:        []string{"image1"},
+	Address: protocols.Address{
+		City:         "city",
+		Neighborhood: "aa",
+		Geolocation: protocols.Geolocation{
+			Precision: "GEO",
+			Location: protocols.Location{
+				Lat: -23.553723,
+				Lon: -46.631146,
+			},
+		},
+	},
+	Bathrooms: 1,
+	Bedrooms:  1,
+	Pricinginfos: protocols.PricingInfos{
+		Price:            45000,
+		Yearlyiptu:       11233,
+		RentalTotalPrice: 12,
+		Businesstype:     "SALE",
+		Monthlycondofee:  123131,
+	},
+}
