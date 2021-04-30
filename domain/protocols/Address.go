83505b71c2326df7fed540a63850a5c359e63ed2
Elisson Alves Silva <elissonalvessilva@192.168.1.3>
2021-04-29 22:42:00 -0400

    refactor: change to a new struct

--- domain/protocols/Address.go
+++ domain/protocols/Address.go
@@ -1,13 +1,17 @@
 package protocols
 
 type Address struct {
-	City         string `json:"city"`
-	Neighborhood string `json:"neighborhood"`
-	Geolocation  struct {
-		Precision string `json:"precision"`
-		Location  struct {
-			Lon float64 `json:"lon"`
-			Lat float64 `json:"lat"`
-		} `json:"location"`
-	} `json:"geoLocation"`
+	City         string      `json:"city"`
+	Neighborhood string      `json:"neighborhood"`
+	Geolocation  Geolocation `json:"geoLocation"`
+}
+
+type Geolocation struct {
+	Precision string   `json:"precision"`
+	Location  Location `json:"location"`
+}
+
+type Location struct {
+	Lon float64 `json:"lon"`
+	Lat float64 `json:"lat"`
 }
